0f57350948a2607d1b5178af0c8dca5e0571c339
刘伟 <dawei.liu@dbappsecurity.com.cn>
2022-05-25 10:04:10 +0800

    feature: 数组功能实现

---
+++ array/my_array.go
@@ -0,0 +1,238 @@
+package array
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+	"sync"
+)
+
+var _ Array = new(MyArray)
+
+type MyArray struct {
+	lock   sync.RWMutex
+	data   []int
+	length int
+	size   int
+}
+
+func New(sizes ...int) (Array, error) {
+	size := -1
+	if len(sizes) > 0 {
+		if sizes[0] > 0 {
+			size = sizes[0]
+		} else {
+			return nil, errors.New("size must be greater than 0")
+		}
+	}
+
+	return &MyArray{
+		size:   size,
+		data:   []int{},
+		length: 0,
+	}, nil
+}
+
+func (m *MyArray) Add(elem int) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.size >= 0 && m.length+1 > m.size {
+		return errors.New("the current data volume has reached the maximum")
+	}
+
+	m.data = append(m.data, elem)
+	m.length++
+	return nil
+}
+
+func (m *MyArray) Insert(index, elem int) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.size >= 0 && m.length+1 > m.size {
+		return errors.New("the current data volume has reached the maximum")
+	}
+
+	if index < 0 || (index != 0 && index > m.length) {
+		text := fmt.Sprintf("index must be between [0:%d(length))", m.length)
+		return errors.New(text)
+	}
+
+	var data []int
+	data = append(data, m.data[:index]...)
+	data = append(data, elem)
+	data = append(data, m.data[index:]...)
+	m.data = data
+	m.length++
+	return nil
+}
+
+func (m *MyArray) Remove(index int) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.length <= 0 {
+		return errors.New("cannot delete an element from an empty array")
+	}
+
+	if index < 0 || index > m.length-1 {
+		text := fmt.Sprintf("index must be between [0:%d(length))", m.length)
+		return errors.New(text)
+	}
+
+	var data []int
+	data = append(data, m.data[:index]...)
+	data = append(data, m.data[index+1:]...)
+	m.data = data
+	m.length--
+	return nil
+}
+
+func (m *MyArray) Delete(elem int) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.length <= 0 {
+		return errors.New("cannot delete an element from an empty array")
+	}
+
+	var data []int
+	for i := 0; i < m.length; i++ {
+		if m.data[i] != elem {
+			data = append(data, m.data[i])
+			m.length--
+		}
+	}
+	m.data = data
+	return nil
+}
+
+func (m *MyArray) Pop() (int, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.length <= 0 {
+		return 0, errors.New("cannot delete an element from an empty array")
+	}
+
+	elem := m.data[m.length-1]
+	m.data = m.data[:m.length-1]
+	m.length--
+	return elem, nil
+}
+
+func (m *MyArray) Set(index, elem int) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.length <= 0 {
+		return errors.New("cannot set an element from an empty array")
+	}
+
+	if index < 0 || index > m.length-1 {
+		text := fmt.Sprintf("index must be between [0:%d(length))", m.length)
+		return errors.New(text)
+	}
+
+	m.data[index] = elem
+	return nil
+}
+
+func (m *MyArray) Resize(size int) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if size < 0 {
+		return errors.New("size must be greater than 0")
+	}
+	m.size = size
+	return nil
+}
+
+func (m *MyArray) Get(index int) (int, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	if m.length <= 0 {
+		return 0, errors.New("cannot set an element from an empty array")
+	}
+
+	if index < 0 || index > m.length-1 {
+		text := fmt.Sprintf("index must be between [0:%d(length))", m.length)
+		return 0, errors.New(text)
+	}
+
+	return m.data[index], nil
+}
+
+func (m *MyArray) Find(elem int) (int, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	if m.length <= 0 {
+		return 0, errors.New("cannot find an element from an empty array")
+	}
+
+	for i := 0; i < m.length; i++ {
+		if m.data[i] == elem {
+			return i, nil
+		}
+	}
+
+	return -1, nil
+}
+
+func (m *MyArray) Tail() (int, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	if m.length <= 0 {
+		return 0, errors.New("cannot get an element from an empty array")
+	}
+
+	return m.data[m.length-1], nil
+}
+
+func (m *MyArray) Head() (int, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	if m.length <= 0 {
+		return 0, errors.New("cannot get an element from an empty array")
+	}
+
+	return m.data[0], nil
+}
+
+func (m *MyArray) Sort() (Array, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.length <= 0 {
+		return &MyArray{
+			size:   m.size,
+			data:   []int{},
+			length: 0,
+		}, nil
+	}
+
+	data := make([]int, m.length)
+	copy(data, m.data)
+
+	sort.Ints(data)
+	return &MyArray{
+		size:   m.size,
+		data:   data,
+		length: m.length,
+	}, nil
+}
+
+func (m *MyArray) Data() []int {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	data := make([]int, m.length)
+	copy(data, m.data)
+	return data
+}
---
+++ array/my_array_test.go
@@ -0,0 +1,99 @@
+package array
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	// 限制大小的数组
+	arr, err := New(3)
+	assert.Nil(t, err)
+
+	// 测试能否正常添加
+	err = arr.Add(1)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{1})
+
+	// 测试超出size大小报错
+	err = arr.Add(2)
+	assert.Nil(t, err)
+	err = arr.Add(3)
+	assert.Nil(t, err)
+	err = arr.Add(4)
+	assert.ErrorContains(t, err, "the current data volume has reached the maximum")
+	assert.Equal(t, arr.Data(), []int{1, 2, 3})
+
+	// 不限制大小的数组
+	arr, err = New()
+	assert.Nil(t, err)
+	err = arr.Add(1)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{1})
+}
+
+func TestData(t *testing.T) {
+	arr, err := New(3)
+	assert.Nil(t, err)
+
+	// 测试获取数组结果正确
+	err = arr.Add(1)
+	assert.Nil(t, err)
+	err = arr.Add(2)
+	assert.Nil(t, err)
+	err = arr.Add(3)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{1, 2, 3})
+}
+
+func TestInsert(t *testing.T) {
+	// 限制大小的数组
+	arr, err := New(4)
+	assert.Nil(t, err)
+
+	// 测试能否正常添加
+	err = arr.Insert(0, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{1})
+
+	// 插入头部位置
+	err = arr.Insert(0, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{2, 1})
+
+	// 插入中间位置
+	err = arr.Insert(1, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{2, 3, 1})
+
+	// 插入尾部位置
+	err = arr.Insert(3, 4)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{2, 3, 1, 4})
+
+	// 测试超出size大小报错
+	err = arr.Insert(0, 1)
+	assert.ErrorContains(t, err, "the current data volume has reached the maximum")
+	assert.Equal(t, arr.Data(), []int{2, 3, 1, 4})
+
+	// 测试插入位置小于0
+	// 删除最后一个元素
+	_, err = arr.Pop()
+	assert.Nil(t, err)
+	err = arr.Insert(-1, 1)
+	assert.ErrorContains(t, err, "index must be between")
+	assert.Equal(t, arr.Data(), []int{2, 3, 1})
+
+	// 测试插入位置大于数组长度
+	err = arr.Insert(5, 1)
+	assert.ErrorContains(t, err, "index must be between")
+	assert.Equal(t, arr.Data(), []int{2, 3, 1})
+
+	// 不限制大小的数组
+	arr, err = New()
+	assert.Nil(t, err)
+	err = arr.Add(1)
+	assert.Nil(t, err)
+	assert.Equal(t, arr.Data(), []int{1})
+}
